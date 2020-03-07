02afd40105c2b58c762fc47d053d5db390855f48
Aprextext <50028112+Aprextext@users.noreply.github.com>
2020-03-07 18:32:38 +0000

    Close camera each time

--- cv-tests/Multicamtest.py
+++ cv-tests/Multicamtest.py
@@ -11,12 +11,12 @@ gp.setup(7,gp.OUT)
 gp.setup(11,gp.OUT)
 gp.setup(12,gp.OUT)
 gp.setup(13,gp.OUT)
-camera=PiCamera()
-camera.resolution=(3280,2464)
 
 def camera1photo():
     i2c = "i2cset -y 1 0x70 0x00 0x04"
     os.system(i2c)
+    camera=PiCamera()
+    camera.resolution=(3280,2464)
     gp.output(7, False)
     gp.output(11, False)
     gp.output(12, True)
@@ -24,24 +24,29 @@ def camera1photo():
     sleep(2)
     camera.capture('camera1.jpg')
     camera.stop_preview()
-    print("Camera A")
+    camera.close()
+    print("Photo Taken on Camera 1")
 
     
 def camera1preview():
     i2c = "i2cset -y 1 0x70 0x00 0x04"
     os.system(i2c)
+    camera=PiCamera()
+    camera.resolution=(640,480)
     gp.output(7, False)
     gp.output(11, False)
     gp.output(12, True)
     camera.start_preview(fullscreen=False,window=(0,0,640,480))
-    print("Camera A")
-
-def cameraend():
+    os.system('read -sn 1 -p "Press any key to continue..."')
     camera.stop_preview()
+    camera.close()
+    
 
 def camera2photo():
     i2c = "i2cset -y 1 0x70 0x00 0x06"
     os.system(i2c)
+    camera=PiCamera()
+    camera.resolution=(3280,2464)
     gp.output(7, False)
     gp.output(11, True)
     gp.output(12, False)
@@ -49,23 +54,27 @@ def camera2photo():
     sleep(2)
     camera.capture('camera2.jpg')
     camera.stop_preview()
-    print("Camera A")
+    print("Photo Taken on Camera 1")
+    camera.close()
     
 def camera2preview():
     i2c = "i2cset -y 1 0x70 0x00 0x06"
     os.system(i2c)
+    camera=PiCamera()
+    camera.resolution=(640,480)
     gp.output(7, False)
     gp.output(11, True)
     gp.output(12, False)
     camera.start_preview(fullscreen=False,window=(0,0,640,480))
     print("Camera A")
-    
-def camerastop():
+    os.system('read -sn 1 -p "Press any key to continue..."')
+    camera.stop_preview()
     camera.close()
+    
 
 def main():
-   userin=""
-   while userin != "end":
+    userin=""
+    while userin != "end":
         userin=input("Command: ")
         if userin=='p1':
             camera1photo()
@@ -79,14 +88,11 @@ def main():
             cameraend()
         if userin=='h':
             print ("photo1 - take photo with camera 1")
-            print ("p1 - start preview on camera 1")
+            print ("c1 - start preview on camera 1")
             print ("photo2 - take photo with camera 2")
-            print ("p2 - start preview on camera 2")
-            print ("s - stop preview (either camera)")
-            print("cs - close camera")
-        if userin=="cs":
-            camerastop()
-camera.close()
+            print ("c2 - start preview on camera 2")
+    
+    camera.close()
 
     
 if __name__ == "__main__":
