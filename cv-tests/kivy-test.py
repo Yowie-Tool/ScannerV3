2e1738899a826be53f7c5d562c6f1ff3fb494fd9
Aprextext <50028112+Aprextext@users.noreply.github.com>
2020-03-14 13:03:58 +0000

    kivy-test again

--- cv-tests/kivy-test.py
+++ cv-tests/kivy-test.py
@@ -1,12 +1,15 @@
 import kivy
-kivy.require('1.10.1')
+kivy.require('1.10.1') # replace with your current kivy version !
+
 from kivy.app import App
 from kivy.uix.label import Label
 
+
 class MyApp(App):
 
-	def build(self):
-		return Label(test='Hello world')
-		
-if __name__ == "__main__":
-	MyApp().run()
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
