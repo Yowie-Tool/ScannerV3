83e93a7227bdd8fe55e56996b0a345743ba329b4
Aprextext <50028112+Aprextext@users.noreply.github.com>
2020-04-01 13:32:42 +0100

    Forgot to add in the control for the camera multiplexer...

--- cv-tests/cvdirectcapture.py
+++ cv-tests/cvdirectcapture.py
@@ -2,6 +2,17 @@ import time
 import picamera
 import numpy as np
 import cv2
+from gpiozero import LED
+import os
+cselect=LED(4)
+cenable1=LED(17)
+cenable2=LED(18)
+laser=LED(20)
+i2c='i2cset -y 1 0x70 0x00 0x04'
+os.system(i2c)
+cselect.off()
+cenable1.off()
+cenable2.on()
 with picamera.PiCamera() as camera:
     camera.resolution = (3280,2464)
     camera.framerate = 24
