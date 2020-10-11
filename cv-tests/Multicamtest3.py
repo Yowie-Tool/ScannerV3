18f87df84db5e585b07404be4f8d22b8f16446e9
Aprextext <50028112+Aprextext@users.noreply.github.com>
2020-10-11 13:04:53 +0100

    Updated for new pin config

--- cv-tests/Multicamtest3.py
+++ cv-tests/Multicamtest3.py
@@ -1,53 +1,34 @@
-from gpiozero import LED
+import RPi.GPIO as GPIO
 import os
 from picamera import PiCamera
 import time
-cselect=LED(4)
-cenable1=LED(17)
-cenable2=LED(18)
-laser=LED(20)
+chan_listc=[12,16,18] #camera switcher pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(self.chan_listc, GPIO.OUT)
 i2c='i2cset -y 1 0x70 0x00 0x04'
 os.system(i2c)
-cselect.off()
-cenable1.off()
-cenable2.on()
+GPIO.output(self.chan_listc,(1,0,0))
+chan_listl=[29,31,33] #laser pins
+GPIO.output(self.chan_listl,0)
 camera=PiCamera()
 camera.resolution=(640,480)
 capnum=0
 
 def camera1():
-    cselect.off()
-    cenable1.off()
-    cenable2.on()
+    GPIO.output(self.chan_listc,(1,0,0))
     i2c='i2cset -y 1 0x70 0x00 0x04'
     os.system(i2c)
     
-def camera2():
-    cselect.on()
-    cenable1.off()
-    cenable2.on()
-    i2c='i2cset -y 1 0x70 0x00 0x05'
-    os.system(i2c)
-    
 def camera3():
-    cselect.off()
-    cenable1.on()
-    cenable2.off()
+    GPIO.output(self.chan_listc,(0,1,0))
     i2c='i2cset -y 1 0x70 0x00 0x06'
-    os.system(i2c)
-    
-def camera4():
-    cselect.on()
-    cenable1.on()
-    cenable.off()
-    i2c='i2cset -y 1 0x70 0x00 0x07'
-    os.system(i2c)
+    os.system(i2c)    
 
 def ccheck():
     camera._check_camera_open()
     
 def cpreview():
-    camera.start_preview(fullscreen=False,window=(0,0,640,480))
+    camera.start_preview(fullscreen=False,window=(200,80,600,400))
     print("camera active")
     
 def cstop():
@@ -63,7 +44,7 @@ def capture():
     print("captured in %d seconds"%(endtime-starttime))
     
 def laserc():
-    laser.toggle()
+    GPIO.output(self.chan_listl,1)
     
 def main():
     userin=""
@@ -71,12 +52,8 @@ def main():
         userin=input("Command: ")
         if userin=='c1':
             camera1()
-        if userin=='c2':
-            camera2()
         if userin=='c3':
             camera3()
-        if userin=='c4':
-            camera4()
         if userin=='p':
             cpreview()
         if userin=='s':
@@ -89,9 +66,7 @@ def main():
             laserc()
         if userin=='h':
             print ("c1 - select camera 1")
-            print ("c2 - select camera 2")
             print ("c3 - select camera 3")
-            print ("c4 - select camera 4")
             print ("cc - Camera check")
             print ("p - Preview")
             print ("s - Stop Preview")
